ffc594fa4ebd1382f1880e605e62b1a51bbea6f6
Rebecca Mamane <rmama012@uottawa.ca>
2019-11-17 14:13:10 -0500

    Bug fix in Sign Up page

--- ClinicApp/app/src/main/java/com/a19_21/clinicapp/controller/SignInActivity.java
+++ ClinicApp/app/src/main/java/com/a19_21/clinicapp/controller/SignInActivity.java
@@ -111,7 +111,6 @@ public class SignInActivity extends AppCompatActivity implements AdapterView.OnI
 
         final String email = emailInput.getText().toString();
         final String password = passwordInput.getText().toString();
-        final String confirmPassword = passwordInput2.getText().toString();
 
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -144,14 +143,14 @@ public class SignInActivity extends AppCompatActivity implements AdapterView.OnI
         String username = usernameInput.getText().toString();
         String email = emailInput.getText().toString();
         String password = passwordInput.getText().toString();
-        String confirmPassword = passwordInput.getText().toString();
+        String confirmPassword = passwordInput2.getText().toString();
         String userType = typeAccount.getSelectedItem().toString();
 
         String id = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-        if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(confirmPassword) && password == confirmPassword) {
+        if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(confirmPassword) && password.equals(confirmPassword)) {
             // Creates different account types
-            User user = new User(id, username, email, confirmPassword, userType);
+            User user = new User(id, username, email, password, userType);
             databaseUsers.child(id).setValue(user);
             return true;
 

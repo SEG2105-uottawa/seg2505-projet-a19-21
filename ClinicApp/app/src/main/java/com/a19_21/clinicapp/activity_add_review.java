84a50ac9d95ddb19df20b18f8787bc263b9f7928
notshravan <svyas050@uottawa.ca>
2019-12-04 02:01:32 +0400

    first commit

---
+++ ClinicApp/app/src/main/java/com/a19_21/clinicapp/activity_add_review.java
@@ -0,0 +1,14 @@
+package com.a19_21.clinicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_add_review extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_review);
+    }
+}
---
+++ ClinicApp/app/src/main/java/com/a19_21/clinicapp/activity_search_by_service_box.java
@@ -0,0 +1,14 @@
+package com.a19_21.clinicapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_search_by_service_box extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_by_service_box);
+    }
+}

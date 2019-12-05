a9075c368d9e756e1c93f34368d07f16e7a604e1
notshravan <svyas050@uottawa.ca>
2019-12-05 08:10:47 +0400

    last commit

--- ClinicApp/app/src/main/java/com/a19_21/clinicapp/model/Review.java
+++ ClinicApp/app/src/main/java/com/a19_21/clinicapp/model/Review.java
@@ -1,4 +1,38 @@
 package com.a19_21.clinicapp.model;
 
 public class Review {
+
+
+    private int Score;
+    private String username;
+    private User user;
+    private String id;
+    private Service service;
+
+
+
+    public Review (int Score, User user,Service service){
+        this.Score = Score;
+        this.user = user;
+        this.service = service;
+
+        username = user.getUsername();
+    }
+
+
+    public void setScore(int score){
+        this.Score = score;
+    }
+
+    public int getScore(){
+        return Score;
+    }
+
+    public Service getreviewedservice(){
+
+        return service;
+    }
+    public User getUserReviewing(){
+        return user;
+    }
 }
